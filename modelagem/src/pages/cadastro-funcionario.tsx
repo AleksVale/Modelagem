feb7be43ad33f723c1041b13730579b0d49e1ec8
CaduMeints <102827368+CaduMeints@users.noreply.github.com>
2023-05-18 12:17:02 -0300

    redefinir senha

--- modelagem/src/pages/cadastro-funcionario.tsx
+++ modelagem/src/pages/cadastro-funcionario.tsx
@@ -61,7 +61,7 @@ export default function CadastroFuncionario() {
             <SimpleInput
               nameInput="nomeCompleto"
               label="Nome Completo"
-              placeholder='Insira o nome'
+              placeholder="Insira o nome"
               value={formData.nomeCompleto}
               onChangeValue={handleOnChangeInput}
             />
@@ -69,14 +69,14 @@ export default function CadastroFuncionario() {
               label="Nome Usuário"
               value={formData.nomeUsuario}
               nameInput="nomeUsuario"
-              placeholder='Insira o nome de usuário'
+              placeholder="Insira o nome de usuário"
               onChangeValue={handleOnChangeInput}
             />
             <SimpleInput
               nameInput="senha"
               label="Senha"
               type="password"
-              placeholder='Insira a senha'
+              placeholder="Insira a senha"
               value={formData.senha}
               onChangeValue={handleOnChangeInput}
             />
@@ -84,21 +84,21 @@ export default function CadastroFuncionario() {
               nameInput="confirmarSenha"
               label="Confirmar senha"
               type="password"
-              placeholder='Insira a senha'
+              placeholder="Insira a senha"
               value={formData.confirmarSenha}
               onChangeValue={handleOnChangeInput}
             />
             <SimpleInput
               nameInput="cargo"
               label="Selecione o cargo"
-              placeholder='Selecione o cargo'
+              placeholder="Selecione o cargo"
               value={formData.cargo}
               onChangeValue={handleOnChangeInput}
             />
             <SimpleInput
               nameInput="cpfCro"
               label="CPF/CRO"
-              placeholder='Insira o CPF ou CRO'
+              placeholder="Insira o CPF ou CRO"
               value={formData.cpfCro}
               onChangeValue={handleOnChangeInput}
             />
